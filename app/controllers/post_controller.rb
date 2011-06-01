7551a3557636d9326e32e378602a8a53518e34a8
thiagobrandam <thiago@thiagobrandam.local>
2011-06-01 13:58:06 -0300

    post response

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,7 +2,10 @@ class PostController < ActionController::Base
   
   def validate
     Post.create(:post_request => request.request_parameters.inspect)
-    render :nothing => true
+    render :text => "ok"
   end
+  
+  
+  # curl -D - -d "studentCode=123456" http://furious-rain-254.heroku.com
   
 end
